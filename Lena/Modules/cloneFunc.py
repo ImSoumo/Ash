39e301af8f29dc629c2cd62fdc97ea7564bb1066
Sᴏᴜᴍᴏ <134998475+its-soumo@users.noreply.github.com>
2024-01-21 14:36:32 +0530

    Updated !

--- Lena/Modules/cloneFunc.py
+++ Lena/Modules/cloneFunc.py
@@ -10,9 +10,9 @@ async def clone(app: Client, message: Message):
     Text = await message.reply("**Usᴀɢᴇ :** `/clone` **ʙᴏᴛ ᴛᴏᴋᴇɴ**")
     Token = message.text.split(None, 1)[1].strip()
     try:
-        await Text.edit("Booting Your Client...")                   
+        await Text.edit("**Booting Your Client...**")                   
         pbot = Client(
-          ":memory:",
+          "Lena",
           api_id=API_ID,
           api_hash=API_HASH,
           bot_token=Token,
@@ -20,6 +20,6 @@ async def clone(app: Client, message: Message):
         )
         await pbot.start()
         user = await pbot.get_me()
-        await message.reply(f"Your Client Has Been Successfully Started As @{user.username}!\nThanks For Cloning.")
+        await message.reply(f**"Your Client Has Been Successfully Started As @{user.username}!**\n**Thanks For Cloning.**")
     except Exception as e:
-        await msg.reply(f"**Error :** `{str(e)}`\nPress /start To Start Again !")
+        await msg.reply(f"**Error :** `{str(e)}`\n**Press /start To Start Again !**")
