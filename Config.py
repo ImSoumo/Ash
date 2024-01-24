91fb916528a86f01ce48795523962725a3709ea0
Sᴏᴜᴍᴏ <134998475+its-soumo@users.noreply.github.com>
2024-01-24 18:44:59 +0530

    Auto-Fixed !

--- Config.py
+++ Config.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6694862755:AAGjQ8gCVFYS9BHBGXYqz264FOgDzq5l2x4")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6533944592:AAHAT5e3tjRy-qJBArPNUwYc3xT9hIhk3Vg")
 API_ID = int(os.environ.get("API_ID", "26850449"))
 API_HASH = os.environ.get("API_HASH", "72a730c380e68095a8549ad7341b0608")
 SUDO_USERS = list(map(int, os.environ.get("SUDO_USERS", "5896960462 1916369262 6517565595").split()))
