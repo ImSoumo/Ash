849518de13eec1743305aebb290db335ef5059d5
Sᴏᴜᴍᴏ <134998475+ItsSoumo@users.noreply.github.com>
2024-04-24 14:20:42 +0530

    Aᴅᴅᴇᴅ :(

--- Ash/__init__.py
+++ Ash/__init__.py
@@ -1,55 +1,17 @@
-import asyncio
-import Config
-import time
+import Anony
 import logging
-from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
-from pyrogram import Client, filters
-from pyrogram.types import Message
-
-MOD_LOAD = []
-MOD_NOLOAD = []
-SUDOERS = filters.user()
-StartTime = time.time()
-
-FORMAT = "[LenaAi] %(message)s"
-logging.basicConfig(
-    handlers=[logging.FileHandler("logs.txt"), logging.StreamHandler()],
-    level=logging.INFO,
-    format=FORMAT,
-    datefmt="[%X]",
-)
-LOGGER = logging.getLogger('[LenaAi]')
-LOGGER.info("LenaAi Starting | Licensed Under BSD 3-Clause License")
-LOGGER.info("Project Maintained By: https://github.com/its-soumo")
-
-Mongo = MongoClient(Config.DB_URL)
-db = Mongo.Lena
-
-async def getSudoers():
-    global SUDOERS
-    LOGGER.info("Loading Sudoers...")
-    sudodb = db.sudo
-    sudoers = await sudodb.find_one({"sudo": "sudo"})
-    sudoers = [] if not sudoers else sudoers["sudoers"]
-    for user_id in Config.SUDO_USERS:
-        SUDOERS.add(user_id)
-        if user_id not in sudoers:
-            sudoers.append(user_id)
-            await sudodb.update_one(
-                {"sudo": "sudo"},
-                {"$set": {"sudoers": sudoers}},
-                upsert=True,
-            )
-    if sudoers:
-        for user_id in sudoers:
-            SUDOERS.add(user_id)
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(getSudoers())
-
-
+from pyrogram import enums
 from pymongo import MongoClient
+from pyrogram.client import Client
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="[Asʜ-Kᴇᴛᴄʜᴜᴍ] %(message)s"
+    handlers=[logging.FileHandler("logs.txt"), logging.StreamHandler()]
+)
+LOGGER = logging.getLogger("[Asʜ-Kᴇᴛᴄʜᴜᴍ]")
+LOGGER.info("Asʜ-Kᴇᴛᴄʜᴜᴍ Sᴛᴀʀᴛɪɴɢ | Lɪᴄᴇɴsᴇᴅ Uɴᴅᴇʀ MIT Lɪᴄᴇɴsᴇ.")
+LOGGER.info("Mᴀɪɴᴛᴀɪɴᴇᴅ Bʏ: Qᴜᴀʀᴛɴʏ")
 
 app = Client(
     name="Asʜ-Kᴇᴛᴄʜᴜᴍ",
@@ -61,22 +23,3 @@ app = Client(
     mongodb=dict(connection=MongoClient(Anony.MONGO_URI), remove_peers=False),
     max_concurrent_transmissions=Anony.MAX_CONCURRENT_TRANSMISSIONS
 )
-
-
-
-app = Client(
-    "Lena",
-    bot_token=Config.BOT_TOKEN,
-    api_id=Config.API_ID,
-    api_hash=Config.API_HASH
-)
-
-LOGGER.info("Starting Client...")
-app.start()
-
-Pyro = app.get_me()
-BOT_ID = Pyro.id
-BOT_NAME = Pyro.first_name + (Pyro.last_name or "")
-BOT_USERNAME = Pyro.username
-BOT_MENTION = Pyro.mention
-BOT_DC_ID = Pyro.dc_id
