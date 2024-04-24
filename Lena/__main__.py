68386e35f0eb1dd1e0d3927b8a4d084689e94432
Sᴏᴜᴍᴏ <134998475+ItsSoumo@users.noreply.github.com>
2024-04-24 14:23:21 +0530

    Update __main__.py

--- Lena/__main__.py
+++ Lena/__main__.py
@@ -1,18 +1,7 @@
-import Config
-import asyncio
-import importlib
-from pyrogram import idle
-from Lena import app, LOGGER
-from Lena.Modules import ALL_MODULES
-
-loop = asyncio.get_event_loop()
-
-async def lenaStart():
-    for all_module in ALL_MODULES:
-        importlib.import_module("Lena.Modules." + all_module)
-    LOGGER.info("Deployed Successfully !")
-    await idle()
-    LOGGER.info("Goodbye LenaAi Is Now Dead !")
+import Anony
+from Ash import app
 
 if __name__ == "__main__":
-    loop.run_until_complete(lenaStart())
+    app.run()
+    with app:
+        app.send_message()
