778cf5cb5495c9d70e0934e68af0eaa5e462589d
Sᴏᴜᴍᴏ <134998475+ItsSoumo@users.noreply.github.com>
2024-04-24 14:41:47 +0530

    Aᴅᴅᴇᴅ :(

--- Ash/__main__.py
+++ Ash/__main__.py
@@ -1,7 +1,9 @@
-import Anony
+import uvloop
 from Ash import app
+from Anony import LOGGER_GROUP
 
 if __name__ == "__main__":
+    uvloop.install()
     app.run()
     with app:
-        app.send_message(Anony.LOGGER_GROUP, "Asʜ Kᴇᴛᴄʜᴜᴍ Hᴀs Bᴇᴇɴ Sᴜᴄᴄᴇssғᴜʟʟʏ Aᴡᴀᴋᴇɴᴇᴅ !")
+        app.send_message(LOGGER_GROUP, "Asʜ Kᴇᴛᴄʜᴜᴍ Hᴀs Bᴇᴇɴ Sᴜᴄᴄᴇssғᴜʟʟʏ Aᴡᴀᴋᴇɴᴇᴅ !")
