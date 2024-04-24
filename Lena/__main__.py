2e4bdae371a0286cdd54837931e65728f1241902
Sᴏᴜᴍᴏ <134998475+ItsSoumo@users.noreply.github.com>
2024-04-24 14:26:04 +0530

    Aᴅᴅᴇᴅ :(

--- Lena/__main__.py
+++ Lena/__main__.py
@@ -4,4 +4,4 @@ from Ash import app
 if __name__ == "__main__":
     app.run()
     with app:
-        app.send_message()
+        app.send_message(Anony.LOGGER_GROUP, "Asʜ Kᴇᴛᴄʜᴜᴍ Hᴀs Bᴇᴇɴ Sᴜᴄᴄᴇssғᴜʟʟʏ Aᴡᴀᴋᴇɴᴇᴅ !")
