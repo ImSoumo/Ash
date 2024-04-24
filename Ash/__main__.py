7c61bc54ae05a33580d9125719aaa8a8dff638a3
Sᴏᴜᴍᴏ <134998475+ItsSoumo@users.noreply.github.com>
2024-04-24 19:50:50 +0530

    Aᴅᴅᴇᴅ :(

--- Ash/__main__.py
+++ Ash/__main__.py
@@ -3,4 +3,5 @@ from Anony import LOGGER_GROUP
 
 if __name__ == "__main__":
     app.run()
-    app.send_message(LOGGER_GROUP, "Asʜ Kᴇᴛᴄʜᴜᴍ Hᴀs Bᴇᴇɴ Sᴜᴄᴄᴇssғᴜʟʟʏ Aᴡᴀᴋᴇɴᴇᴅ !")
+    with app:
+        app.send_message(LOGGER_GROUP, "Asʜ Kᴇᴛᴄʜᴜᴍ Hᴀs Bᴇᴇɴ Sᴜᴄᴄᴇssғᴜʟʟʏ Aᴡᴀᴋᴇɴᴇᴅ !")
