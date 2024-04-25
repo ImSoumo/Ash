4898b5147431e7074a9bad2fc8b5f1c0b1e3380f
Sᴏᴜᴍᴏ <134998475+ItsSoumo@users.noreply.github.com>
2024-04-25 20:51:04 +0530

    ::

---
+++ Ash/DB.py
@@ -0,0 +1,63 @@
+from Modules.rankings import db
+
+cleandb = db.cleanmode
+cleanmode = {}
+
+
+async def is_cleanmode_on(chat_id: int) -> bool:
+    mode = cleanmode.get(chat_id)
+    if not mode:
+        user = await cleandb.find_one({"chat_id": chat_id})
+        if not user:
+            cleanmode[chat_id] = True
+            return True
+        cleanmode[chat_id] = False
+        return False
+    return mode
+
+
+async def cleanmode_on(chat_id: int):
+    cleanmode[chat_id] = True
+    user = await cleandb.find_one({"chat_id": chat_id})
+    if user:
+        return await cleandb.delete_one({"chat_id": chat_id})
+
+
+async def cleanmode_off(chat_id: int):
+    cleanmode[chat_id] = False
+    user = await cleandb.find_one({"chat_id": chat_id})
+    if not user:
+        return await cleandb.insert_one({"chat_id": chat_id})
+
+
+
+usersdb = db.users
+
+
+async def is_afk(user_id: int) -> bool:
+    user = await usersdb.find_one({"user_id": user_id})
+    if not user:
+        return False, {}
+    return True, user["reason"]
+
+
+async def add_afk(user_id: int, mode):
+    await usersdb.update_one(
+        {"user_id": user_id}, {"$set": {"reason": mode}}, upsert=True
+    )
+
+
+async def remove_afk(user_id: int):
+    user = await usersdb.find_one({"user_id": user_id})
+    if user:
+        return await usersdb.delete_one({"user_id": user_id})
+
+
+async def get_afk_users() -> list:
+    users = usersdb.find({"user_id": {"$gt": 0}})
+    if not users:
+        return []
+    users_list = []
+    for user in await users.to_list(length=1000000000):
+        users_list.append(user)
+    return users_list
