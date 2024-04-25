464ae5e8b89e1414b440c7c84b2b9f45964d7eb0
Sᴏᴜᴍᴏ <134998475+ItsSoumo@users.noreply.github.com>
2024-04-25 22:06:38 +0530

    ::

--- Ash/Core.py
+++ Ash/Core.py
@@ -4,7 +4,6 @@ from datetime import datetime, timedelta
 from logging import getLogger
 from typing import Union
 
-import emoji
 from pyrogram.errors import (
     FloodWait,
     InputUserDeactivated,
