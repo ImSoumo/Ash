2cf1adf49b948f145c3ed974b1d68fef428a264d
Sᴏᴜᴍᴏ <134998475+ItsSoumo@users.noreply.github.com>
2024-04-26 11:43:50 +0530

    ::

--- Modules/tests.py
+++ Modules/tests.py
@@ -202,14 +202,14 @@ async def afk_state(Guardian, ctx: Message):
         await ctx.reply("**ᴜꜱᴀɢᴇ :** /afkdel [ᴇɴᴀʙʟᴇ|ᴅɪꜱᴀʙʟᴇ] ᴛᴏ ᴇɴᴀʙʟᴇ ᴏʀ ᴅɪꜱᴀʙʟᴇ ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ ᴍᴇꜱꜱᴀɢᴇ.")
 
 
-@app.on_message(filters.group & filters.bot & filters.via_bot)
+@app.on_message(filters.all & filters.group & filters.bot & filters.via_bot)
 async def afk_watcher_func(self: Client, ctx: Message):
     if ctx.sender_chat:
         return
     userid = ctx.from_user.id
     user_name = ctx.from_user.mention
     if ctx.entities:
-        possible = ["/test", "!test"]
+        possible = "test"
         message_text = ctx.text or ctx.caption
         for entity in ctx.entities:
             try:
